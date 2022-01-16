16fec7f0f5dfb8ef7bf14a76e45ce0b63354ee7d
Dzmitry Semianovich <dima.semenovich.01@icloud.com>
2022-01-16 23:07:58 +0300

    initial commit + auth screen

---
+++ Auction/Auction/App/AuctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuctionApp.swift
+//  Auction
+//
+//  Created by Dzmitry Semenovich on 16.01.22.
+//
+
+import SwiftUI
+
+@main
+struct AuctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
